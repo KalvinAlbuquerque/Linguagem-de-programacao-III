9fe45b677887e3c340b3d1e89ac9fd14502d9516
Kalvin <kalvinalbuquerque5@gmail.com>
2024-04-14 18:31:17 -0300

    Feito client e servidor UDP

--- UDP/ClientUDP.java
+++ UDP/ClientUDP.java
@@ -1,77 +1,46 @@
 package UDP;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientUDP 
 {
-    private byte[] buffer;
-    private InetAddress inetAddress;
-    private DatagramSocket datagramSocket;
-    private Scanner scan;
     
-    public ClientUDP()
+    private static String STOP_STRING = "##";
+    public static int PORT = 5050;
+    public static void main(String[] args)
     {
+        DatagramPacket sendPacket;
+        byte[] sendData;
+
         try
         {
-            this.datagramSocket =    
-            this.scan = new Scanner(System.in);
-            writeMessage();
+            /* Criando o UDP socket */
+            DatagramSocket clientSocket = new DatagramSocket();
+            Scanner scan = new Scanner(System.in);
+            String message = "";
+            while(!message.equals(STOP_STRING))
+            {
+                System.out.println("Digite sua mensagem: ");
+                message = scan.nextLine();
+                sendData = message.getBytes();
+                /* Pacote de dados, tamanho do pacote, IP e porta */
+                sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("127.0.0.1"), ClientUDP.PORT); 
+                clientSocket.send(sendPacket);
+            }
+
+            scan.close();
+            clientSocket.close();
 
         }
         catch(IOException e)
         {
-            System.out.println("Error: " + e.getMessage());
-        }
-        finally
-        {
-            close();
-        }
-
-
-    }
-
-    /* Método responsável por enviar mensagens infinitamente para o servidor. Para tal estou usando o scanner para ler o meu teclado e o DataOutputStream para
-     * enviar a mensagem para o servidor.
-     */
-    private void writeMessage() throws IOException
-    {
-        String message = "";
-        while(!message.equals(ServerUDP.STOP_STRING))
-        {
-            System.out.println("Enter message: ");
-            message = scan.nextLine();
-            buffer = message.getBytes();
-            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length, )
-        }
-
-    }
-
-    /* 
-     * Método responsável por fechar a conexão (socket), scan e o DataOutputStream.
-     */
-    private void close()
-    {
-        try
-        {
-            socket.close();
-            scan.close();
-
-        }
-        catch(Exception e)
-        {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args)
-    {
-        new ClientUDP();
+        
     }
 }
 
--- UDP/ServerUDP.java
+++ UDP/ServerUDP.java
@@ -1,4 +1,5 @@
 package UDP;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,75 +9,39 @@ public class ServerUDP
     public static int PORT = 5050;
     public static String STOP_STRING = "##";
 
-    private DatagramSocket server;
-
-    private byte[] buffer = new byte[256];
-
-    public ServerUDP()
-    {
-        try
-        {
-            /* Criando servidor UDP e passando a porta */
-            this.server = new DatagramSocket(PORT);
-            System.out.println("Server initialized sucessfully");
-
-      
-
-            /* Uma vez recebida a conexão, irei entrar em um looping infinito, em que o servidor ficará lendo todas as mensagens vindas do servidor
-             * até que a mensagem que indica a finalizaçaõ da conexão seja enviada.
-             */
-            if(server.isConnected())
-            {
-                readMessage();
-                close();
-            }
-
-        }
-        catch(IOException e)
-        {
-            System.out.println("Error: " + e.getMessage());
-        }
-
-    }
-
-    /* 
-     * Método responsável por ler todas as mensagens enviadas pelo client.
-     * OBS.:
-     * Estou utilizando o DataInputStream e o DataOutputStream, há o ObjectOutputStream/ObjectInputStream e são, sem dúvidas, bugados. Se possível não os utilize,
-     * deu muito trabalho fazer uma aplicação rodar com eles.
-     * 
-     * Aqui estou acessando o buffer de input, para onde as mensagens do client estão sendo enviadas, lendo os caracteres e armazenando-os em "message".
-     */
-    private void readMessage()
-    {
-        try
-        {
-            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
-            this.server.receive(datagramPacket);
-            String message = "";
-            while(!message.equals(STOP_STRING))
-            {   
-                message = new String(datagramPacket.getData(),0, datagramPacket.getLength());
-                System.out.println("Client said: " + message);
-            }
-        }
-        catch(IOException e)
-        {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-
-    /* 
-     * Método responsável por fechar o servidor e o canal de input.
-     */
-    private void close() throws IOException
-    {
-        server.close();
-    }
-
     public static void main(String[] args)
     {
-        new ServerUDP();
+        try
+        {
+            /* Criando o servidor UDP */
+            DatagramSocket serverSocket = new DatagramSocket(ServerUDP.PORT);
+            System.out.println("Server initialized sucessfully");
+
+            /* Criando buffer para receber os bytes (mensagens) do client */
+            byte[] receivedData = new byte[1024];
+            DatagramPacket receivPacket;
+
+            String message = "";
+            while(!message.equals(ServerUDP.STOP_STRING))
+            {
+                /* Esperando pela mensagem do client */
+                receivPacket = new DatagramPacket(receivedData, receivedData.length);
+                serverSocket.receive(receivPacket);
+
+                message = new String(receivPacket.getData(), 0, receivPacket.getLength());
+
+                System.out.println("Client said: " + message);
+                
+            }
+
+            serverSocket.close();
+        }   
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
+
+  
 }
